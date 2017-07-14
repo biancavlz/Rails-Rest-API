9069746fb50870a24f0e6e3ca0d7d8abad0ac26f
Bianca <ing_biank@hotmail.com>
2017-07-14 16:38:29 +0200

    Adds update action

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -28,8 +28,19 @@ module Api
       def destroy
         article = Article.find(params[:id])
         article.destroy
-        render json: { status: 'SUCCESS', message: 'Destroyed article', data: article }, 
+        render json: { status: 'SUCCESS', message: 'Destroyed article', data: article },
         status: :ok   
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: { status: 'SUCCESS', message: 'Updated article', data: article }, 
+          status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Article not updated', data: article.errors }, 
+          status: :unprocessable_entity
+        end
       end
 
       private
